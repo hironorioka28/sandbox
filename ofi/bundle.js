773f9dff2c5aed9e176868135c63c9d38fe98d50
Hironori Oka <hironorioka28@gmail.com>
2017-04-23 21:17:43 +0900

    constructed

--- ofi/bundle.js
+++ ofi/bundle.js
@@ -46,6 +46,17 @@
 
 	var $ = __webpack_require__(1);
 
+	$(function(){
+	   $('a[href^="#"]').click(function() {
+	      var speed = 1000;
+	      var href= $(this).attr("href");
+	      var target = $(href == "#" || href == "" ? 'html' : href);
+	      var position = target.offset().top;
+	      $('body,html').animate({scrollTop:position}, speed, 'swing');
+	      return false;
+	   });
+	});
+
 
 /***/ },
 /* 1 */
