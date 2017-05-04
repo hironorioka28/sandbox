b81a52d150f8868225cfde3c694b60e768583c53
Hironori Oka <hironorioka28@gmail.com>
2017-05-04 15:01:03 +0900

    updte

--- ofi/bundle.js
+++ ofi/bundle.js
@@ -82,15 +82,11 @@
 
 	$(function() {
 	  $(window).on("load", function() {
-	    //$(".js-videoWrapper").addClass("show");
-	    //$("#video").css({
-	    //  "opacity": 1
-	    //});
-	  });
 	    $(".js-videoWrapper").addClass("show");
 	    $("#video").css({
 	      "opacity": 1
 	    });
+	  });
 
 	  var $video = $("#video").get(0);
 	  var time = 320;
--- ofi/main.js
+++ ofi/main.js
@@ -36,15 +36,11 @@ $(function() {
 
 $(function() {
   $(window).on("load", function() {
-    //$(".js-videoWrapper").addClass("show");
-    //$("#video").css({
-    //  "opacity": 1
-    //});
-  });
     $(".js-videoWrapper").addClass("show");
     $("#video").css({
       "opacity": 1
     });
+  });
 
   var $video = $("#video").get(0);
   var time = 320;
